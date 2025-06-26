b0b6cd0f0b5ceb83eb21b7c9e39a81fb5a1745d6
Jessica Churgin <jchurgy@gmail.com>
2025-06-26 13:32:41 -0400

    formatting strings

--- variables/strings.py
+++ variables/strings.py
@@ -6,3 +6,15 @@ print(name)
 
 greeting = f"Hello, {name}!"
 print(greeting)
+
+
+greeting2 = "Hello, {}"
+name = "Madam Jessica"
+with_name = greeting2.format(name)
+with_name_two = greeting2.format("Dan")
+print(with_name_two)
+
+longer_phrase = "Hello, {} this string demonstrates using multiple placeholders. Today is {} ."
+
+formatted_phrase = longer_phrase.format("Jess", "Monday")
+print(formatted_phrase)
