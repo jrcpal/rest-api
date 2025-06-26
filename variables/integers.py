c83bcaba4012d9b859bdb654913d31c3c4755422
Jessica Churgin <jchurgy@gmail.com>
2025-06-26 13:23:19 -0400

    multiply nums

--- variables/integers.py
+++ variables/integers.py
@@ -5,3 +5,12 @@ discount = 0.2
 
 result = price * (1 - discount)
 print(result)
+
+# Create two variables, var1 and var2, both with the same value.
+var1 = 10
+var2 = 10
+
+# Create two variables, num1 and num2, which multiply together to give 16.
+num1 = 8
+num2 = 2
+print(num1 * num2)
