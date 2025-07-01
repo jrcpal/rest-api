3a6508087181a17c9e01ee0e0838ab67c322a231
Jessica Churgin <jchurgy@gmail.com>
2025-07-01 15:30:11 -0400

    calculate users age in months

---
+++ exercises/userAge.py
@@ -0,0 +1,4 @@
+user_age = int(input("Enter your age: "))
+months = user_age * 12
+
+print(f'You are {months} months old.')
--- input/userInput.py
+++ input/userInput.py
@@ -2,7 +2,7 @@ name = input("Enter your name: ")
 print(f"Hello, {name}!")
 
 
-# prompt that includes math, as input is always a string 
+# prompt that includes math, as input always returns a string 
 size_input = input("How big is your house? (in square feet): ")
 square_feet = int(size_input)
 square_meters = square_feet / 10.764
